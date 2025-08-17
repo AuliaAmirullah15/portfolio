0db8191b3d6fab394c1465ff61a1726ae5012f52
Aulia Amirullah <auliaamir153@gmail.com>
2025-08-17 13:40:16 +0100

    Changed open to work to connections

--- src/components/GeneralFooter.tsx
+++ src/components/GeneralFooter.tsx
@@ -193,7 +193,7 @@ const GeneralFooter = ({
               </button>
               <div className="flex items-center space-x-2">
                 <div className="w-2 h-2 rounded-full bg-green-400 animate-pulse"></div>
-                <span>Available for work</span>
+                <span>Open to new connections</span>
               </div>
             </div>
           </div>
